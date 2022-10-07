ee80da530755dd069754216df7329a356e11768d
rubayetseason <season.ss2001@gmail.com>
2022-10-07 14:03:48 +0600

    router added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../images/Logo.svg'
 import './Header.css'
 
 const Header = () => {
     return (
         <div>
-           <nav className='header'>
-            <img src={logo} />
-            <div>
-                <a href="/shop">Shop</a>
-                <a href="/orders">Orders</a>
-                <a href="/inventory">Inventory</a>
-                <a href="/about">About</a>
-            </div>
-           </nav>
+            <nav className='header'>
+                <img src={logo} />
+                <div>
+                    <Link to="/">Shop</Link>
+                    <Link to="/orders">Orders</Link>
+                    <Link to="/inventory">Inventory</Link>
+                    <Link to="/about">About</Link>
+                </div>
+            </nav>
         </div>
     );
 };
